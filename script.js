98454c29f6ec841b4953d97c8856b9a93fce5a59
rfl07 <ikankanjut1@gmail.com>
2026-07-24 20:15:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efek Navbar berubah shadow saat discroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.boxShadow = '0 10px 30px rgba(0, 0, 0, 0.5)';
+    } else {
+        header.style.boxShadow = 'none';
+    }
+});
